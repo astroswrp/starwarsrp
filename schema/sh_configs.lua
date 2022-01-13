473c995b6bc6fa2a3238120daeb42f3f6009cf3d
Guy <58865103+GuyHero1234@users.noreply.github.com>
2022-01-12 21:09:47 -0500

    fuck me
    asd

--- schema/sh_configs.lua
+++ schema/sh_configs.lua
@@ -6,7 +6,7 @@ ix.currency.plural = "credits"
 ix.config.SetDefault("scoreboardRecognition", true)
 ix.config.SetDefault("music", "music/hl2_song19.mp3")
 ix.config.SetDefault("maxAttributes", 60)
-ix.anim.SetModelClass("models/nada/rogueonetk.mdl", "citizen_male")
+
 
 --[[ix.config.Add("rationTokens", 20, "The amount of tokens that a person will get from a ration", nil, {
 	data = {min = 0, max = 1000},
