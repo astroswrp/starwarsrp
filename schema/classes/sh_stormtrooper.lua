391e065187cf497c51efeb12125a340b492e2642
Guy <58865103+GuyHero1234@users.noreply.github.com>
2022-01-12 21:06:14 -0500

    t-pose thrid time
    fixed??

--- schema/classes/sh_stormtrooper.lua
+++ schema/classes/sh_stormtrooper.lua
@@ -5,7 +5,6 @@ function CLASS:OnSet(client)
 	local character = client:GetCharacter()
 
 	if (character) then
-		ix.anim.SetModelClass("models/nada/rogueonetk.mdl", "citizen_male")
 		character:SetModel("models/nada/rogueonetk.mdl")
 	end
 end
--- schema/sh_configs.lua
+++ schema/sh_configs.lua
@@ -1,13 +1,14 @@
 
 ix.currency.symbol = ""
-ix.currency.singular = "token"
-ix.currency.plural = "tokens"
+ix.currency.singular = "credit"
+ix.currency.plural = "credits"
 
 ix.config.SetDefault("scoreboardRecognition", true)
 ix.config.SetDefault("music", "music/hl2_song19.mp3")
 ix.config.SetDefault("maxAttributes", 60)
+ix.anim.SetModelClass("models/nada/rogueonetk.mdl", "citizen_male")
 
-ix.config.Add("rationTokens", 20, "The amount of tokens that a person will get from a ration", nil, {
+--[[ix.config.Add("rationTokens", 20, "The amount of tokens that a person will get from a ration", nil, {
 	data = {min = 0, max = 1000},
 	category = "economy"
 })
@@ -16,3 +17,4 @@ ix.config.Add("rationInterval", 300, "How long a person needs to wait in seconds
 	data = {min = 0, max = 86400},
 	category = "economy"
 })
+]]--
