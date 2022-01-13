36b2ba858df597fdd924649aee8d3e1fdbb987e8
Guy <58865103+GuyHero1234@users.noreply.github.com>
2022-01-12 21:00:40 -0500

    hope this works

--- schema/classes/sh_stormtrooper.lua
+++ schema/classes/sh_stormtrooper.lua
@@ -5,9 +5,10 @@ function CLASS:OnSet(client)
 	local character = client:GetCharacter()
 
 	if (character) then
+		ix.anim.SetModelClass("models/nada/rogueonetk.mdl", "citizen_male")
 		character:SetModel("models/nada/rogueonetk.mdl")
 	end
 end
-ix.anim.SetModelClass("models/nada/rogueonetk.mdl", "citizen_male")
+
 CLASS.isDefault = false
 CLASS_RECRUIT = CLASS.index
