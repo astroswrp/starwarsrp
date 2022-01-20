ea34ec25cf6ca888d06de28bef81ad11767970bd
Guy <58865103+GuyHero1234@users.noreply.github.com>
2022-01-20 12:35:29 -0500

    fix?
    fixed class and faction hopefully

---
+++ schema/classes/sh_recruit.lua
@@ -0,0 +1,5 @@
+CLASS.name = "9th Infantry Recruit"
+CLASS.faction = FACTION_IGF
+
+CLASS.isDefault = true
+CLASS_RECRUIT = CLASS.index
---
+++ schema/factions/sh_imperialarmy.lua
@@ -0,0 +1,27 @@
+
+FACTION.name = "Imperial Ground Forces"
+FACTION.description = "Members of the Galctic Empire's Ground Forces"
+FACTION.color = Color(150, 125, 100, 255)
+FACTION.isDefault = true
+FACTION.models = {
+	"models/hcn/starwars/bf/human/human_male_5.mdl"
+}
+
+FACTION.weapons = {
+	"weapon_empty_hands"
+}
+function FACTION:OnCharacterCreated(client, character)
+	local id = Schema:ZeroNumber(math.random(1, 99999), 5)
+	local inventory = character:GetInventory()
+
+	character:SetData("cid", id)
+
+	inventory:Add("suitcase", 1)
+	inventory:Add("cid", 1, {
+		name = character:GetName(),
+		id = id
+	})
+end
+FACTION.isGloballyRecognized = true
+FACTION.isDefault = true
+FACTION_IGF = FACTION.index
