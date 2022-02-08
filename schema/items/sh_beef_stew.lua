6ce266b05feb2f061919e296cfa07285b2e21f78
Guy <58865103+GuyHero1234@users.noreply.github.com>
2022-02-08 10:56:49 -0500

    removal of items
    removed some redundant items

--- schema/items/sh_beef_stew.lua
+++ schema/items/sh_beef_stew.lua
@@ -1,11 +1,9 @@
-
 ITEM.name = "Beef Stew"
 ITEM.model = Model("models/illusion/eftcontainers/beefstew.mdl")
 ITEM.width = 1
 ITEM.height = 1
 ITEM.description = "A can of the Empire's finest Beef Stew."
 ITEM.category = "Consumables"
-ITEM.permit = "consumables"
 
 ITEM.functions.Eat = {
 	OnRun = function(itemTable)
