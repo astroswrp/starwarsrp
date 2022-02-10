147083d57953c6d39353db157a52597c344efcf4
Guy <58865103+GuyHero1234@users.noreply.github.com>
2022-02-09 21:05:19 -0500

    more items
    added transparisteel and spice

---
+++ plugins/crafting/items/resources/sh_transparisteel.lua
@@ -0,0 +1,6 @@
+ITEM.name = "Transparisteel"
+ITEM.description = "A reinforced clear material created from Refined Lommite Ore"
+ITEM.model = Model("models/illusion/eftcontainers/plexiglass.mdl")
+ITEM.noBusiness = true 
+ITEM.width = 1
+ITEM.height = 1
