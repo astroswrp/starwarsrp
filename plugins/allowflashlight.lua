be2dde0984e2b5964dbfa3a7a7e175b9b2326c44
Guy <58865103+GuyHero1234@users.noreply.github.com>
2022-03-02 10:41:05 -0500

    plugins
    updated some plugins

--- plugins/allowflashlight.lua
+++
@@ -1,7 +0,0 @@
-PLUGIN.name = "Allow Flashlights"
-PLUGIN.author = "Riggs"
-PLUGIN.description = "Allows everyone to use flashlights."
-
-function PLUGIN:PlayerSwitchFlashlight(client, bEnabled)
-	return true
-end
